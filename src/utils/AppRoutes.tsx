44cde7a8c4a84e76b5b70ffd032f68ec3e113dba
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-10-22 15:14:05 -0300

    refactor: changed FileUpload to Story

--- src/utils/AppRoutes.tsx
+++ src/utils/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import FileUpload from "../components/FileUpload";
+import { Story } from "../pages/Story";
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<FileUpload />} />
+        <Route path="/story" element={<Story />} />
       </Routes>
     </Router>
   );
