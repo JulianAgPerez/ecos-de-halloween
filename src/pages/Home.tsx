3f893d6857f146b57189bb9ab57453345beb9f9b
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-10-22 18:04:39 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useState, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import AmbientSound from "../components/AmbientSound";
 import { sounds } from "../assets/sounds/sounds";
-import ContentSection from "../components/ContentSection";
+import ContentSection from "../components/Home/ContentSection";
 import { DropdownMenu } from "../components/DropdownMenu";
 import VolumeButton from "../components/VolumeButton";
 
