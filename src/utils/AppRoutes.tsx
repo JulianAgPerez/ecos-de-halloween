3d067b7bd7b1b9729c5d3fec42eb826d406815ba
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-10-24 13:37:46 -0300

    ft: added form to upload body

--- src/utils/AppRoutes.tsx
+++ src/utils/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import { Story } from "../pages/Story";
 import SidebarMenu from "../components/SidebarMenu";
 import Footer from "../components/Footer";
+import UploadForm from "../pages/UploadForm";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes: React.FC = () => {
       <SidebarMenu />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/form" element={<UploadForm />} />
         <Route path="/story/:id" element={<Story />} />
       </Routes>
       <Footer />
