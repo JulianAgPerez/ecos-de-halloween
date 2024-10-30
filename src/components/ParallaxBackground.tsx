c446beb611ac40ee14125dd838c9dc1e4e9c05c8
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-10-30 13:22:05 -0300

    fix: ajustada imagen y efecto parallax

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -1,42 +1,37 @@
-// ParallaxBackground.tsx
-import React, { useRef } from "react";
+import { FC, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-interface ParallaxBackgroundProps {
-  backgroundImageUrl?: string;
-  defaultBackgroundClass: string;
-  children: React.ReactNode;
+interface ParallaxProps {
+  text: string;
 }
 
-const ParallaxBackground: React.FC<ParallaxBackgroundProps> = ({
-  backgroundImageUrl,
-  defaultBackgroundClass,
-  children,
-}) => {
-  const ref = useRef<HTMLDivElement | null>(null);
+const ParallaxBackground: FC<ParallaxProps> = ({ text }) => {
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
-
-  // Transformaciones para el parallax
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
   const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
 
   return (
-    <div ref={ref} className="relative w-full h-screen overflow-hidden">
+    <div
+      ref={ref}
+      className="relative h-screen overflow-hidden grid place-items-center"
+    >
+      <motion.h1
+        style={{ y: textY }}
+        className="font-creepster text-white text-6xl md:text-9xl font-bold text-center z-20 relative"
+      >
+        {text}
+      </motion.h1>
       <motion.div
-        className={`absolute inset-0 bg-cover bg-center ${
-          backgroundImageUrl ? "" : defaultBackgroundClass
-        }`}
+        className="absolute inset-0 bg-cover bg-home-principal bg-bottom"
         style={{
-          backgroundImage: backgroundImageUrl
-            ? `url(${backgroundImageUrl})`
-            : undefined,
           y: backgroundY,
         }}
       />
-      {children}
+      <div className="absolute inset-0 z-19 bg-home-piso md:bg-home-secundaria-clean bg-cover bg-bottom" />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,67 @@
+import { useRef, useEffect } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import AmbientSound from "../components/AmbientSound";
+import { sounds } from "../assets/sounds/sounds";
+import ContentSection from "../components/Home/ContentSection";
+import VolumeButton from "../components/VolumeButton";
+import useSoundStore from "../store/useSoundStore";
+
+const Home = () => {
+  const { addSound } = useSoundStore();
+
+  // setup de Parallax
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  // Transformacion para texto (complementario de parallax)
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+
+  useEffect(() => {
+    Object.values(sounds).forEach((sound) => addSound(sound));
+  }, [addSound]);
+
+  return (
+    <div ref={ref}>
+      {Object.values(sounds).map((sound) => (
+        <AmbientSound key={sound} src={sound} />
+      ))}
+      {/* Contenedor con la imagen de fondo y efecto parallax */}
+      <div className="relative h-screen overflow-hidden grid place-items-center ">
+        <motion.h1
+          style={{ y: textY }}
+          className="font-creepster text-white text-6xl md:text-9xl font-bold text-center z-20 relative"
+        >
+          Feliz Halloween
+        </motion.h1>
+
+        <motion.div
+          className="absolute inset-0 bg-home-principal bg-cover bg-bottom"
+          style={{
+            y: backgroundY,
+          }}
+        />
+        <div className=" absolute inset-0 z-19 bg-home-piso md:bg-home-secundaria-clean bg-cover bg-bottom" />
+
+        {/* <DropdownMenu /> */}
+        <VolumeButton />
+      </div>
+      <ContentSection />
+      {/* Sección de "Corteza" No se si agregar imagenes o que */}
+      <div className="layer crust"></div>
+      {/*       <div className="layer hell">Infierno</div>
+       */}{" "}
+    </div>
+  );
+};
+
+export default Home;
+
+/*
+//Alternativa con componente (genera una especie de ruido o linea blanca molesta arriba del todo)
 import { useEffect } from "react";
 import AmbientSound from "../components/AmbientSound";
 import { sounds } from "../assets/sounds/sounds";
@@ -14,17 +78,18 @@ const Home = () => {
   }, [addSound]);
 
   return (
-    <ParallaxBackground defaultBackgroundClass="bg-home-principal">
+    <div>
       {Object.values(sounds).map((sound) => (
         <AmbientSound key={sound} src={sound} />
       ))}
-      <h1 className="font-creepster text-white text-6xl md:text-9xl font-bold text-center z-20 relative">
-        Feliz Halloween
-      </h1>
+      <ParallaxBackground text="Feliz Halloween" />
       <VolumeButton />
       <ContentSection />
-    </ParallaxBackground>
+      <div className="layer crust"></div>
+    </div>
   );
 };
 
 export default Home;
+
+*/
