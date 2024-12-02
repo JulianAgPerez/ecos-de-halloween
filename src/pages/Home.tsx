f2f859b52c9bbeb249b016147dc402ab1052e5a1
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-12-02 13:49:51 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,8 @@ const Home = () => {
 
   // Transformacion para texto (complementario de parallax)
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "700%"]);
+  const textOpacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
 
   useEffect(() => {
     Object.values(sounds).forEach((sound) => addSound(sound));
@@ -30,9 +31,9 @@ const Home = () => {
         <AmbientSound key={sound} src={sound} />
       ))}
       {/* Contenedor con la imagen de fondo y efecto parallax */}
-      <div className="relative h-screen overflow-hidden grid place-items-center ">
+      <div className="relative h-screen overflow-hidden grid place-items-center">
         <motion.h1
-          style={{ y: textY }}
+          style={{ y: textY, opacity: textOpacity }}
           className="font-creepster text-white text-6xl md:text-9xl font-bold text-center z-20 relative"
         >
           Feliz Halloween
@@ -44,52 +45,14 @@ const Home = () => {
             y: backgroundY,
           }}
         />
-        <div className=" absolute inset-0 z-19 bg-home-piso md:bg-home-secundaria-clean bg-cover bg-bottom" />
-
-        {/* <DropdownMenu /> */}
+        <div className="absolute inset-0 z-19 bg-home-piso md:bg-home-secundaria-clean bg-cover bg-bottom" />
         <VolumeButton />
       </div>
       <ContentSection />
       {/* Sección de "Corteza" No se si agregar imagenes o que */}
       <div className="layer crust"></div>
-      {/*       <div className="layer hell">Infierno</div>
-       */}{" "}
     </div>
   );
 };
 
 export default Home;
-
-/*
-//Alternativa con componente (genera una especie de ruido o linea blanca molesta arriba del todo)
-import { useEffect } from "react";
-import AmbientSound from "../components/AmbientSound";
-import { sounds } from "../assets/sounds/sounds";
-import ContentSection from "../components/Home/ContentSection";
-import VolumeButton from "../components/VolumeButton";
-import useSoundStore from "../store/useSoundStore";
-import ParallaxBackground from "../components/ParallaxBackground";
-
-const Home = () => {
-  const { addSound } = useSoundStore();
-
-  useEffect(() => {
-    Object.values(sounds).forEach((sound) => addSound(sound));
-  }, [addSound]);
-
-  return (
-    <div>
-      {Object.values(sounds).map((sound) => (
-        <AmbientSound key={sound} src={sound} />
-      ))}
-      <ParallaxBackground text="Feliz Halloween" />
-      <VolumeButton />
-      <ContentSection />
-      <div className="layer crust"></div>
-    </div>
-  );
-};
-
-export default Home;
-
-*/
